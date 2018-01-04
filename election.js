33dbca2bba9b780791db7002d94e262a16cf73c1
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-04 15:21:30 -0500

    fixed assynchronous voting to not need to refresh page

--- election.js
+++ election.js
@@ -5,21 +5,25 @@ var list = document.querySelector('#list');
     method: 'GET',
     datatype: 'json'
   }).done(function(data){
-    for (i=0; i<data.candidates.length; i++){
+    for (var i=0; i<data.candidates.length; i++){
       var candidate = data.candidates[i];
       var liTag = document.createElement('li');
       var formTag = document.createElement('form');
       var submitButton = document.createElement('input');
       var hiddenButton = document.createElement('input');
+      var voteSpan = document.createElement('span')
+      voteSpan.innerText = candidate.votes
       formTag.method = 'POST';
       formTag.action = 'https://bb-election-api.herokuapp.com/vote';
       submitButton.type = "submit";
+      submitButton.value = "Vote"
       hiddenButton.setAttribute('type', "hidden");
       hiddenButton.setAttribute('name', "name");
       hiddenButton.setAttribute('value', candidate.name);
       formTag.append(submitButton);
       formTag.append(hiddenButton);
-      liTag.innerText = candidate.name + ', Votes: ' + candidate.votes;
+      liTag.innerText = candidate.name + ', Votes: ';
+      liTag.append(voteSpan)
       liTag.append(formTag);
       list.append(liTag);
       formTag.addEventListener('submit', function(event){
@@ -33,7 +37,16 @@ var list = document.querySelector('#list');
           var refreshButton = document.createElement('button');
           refreshButton.innerText = 'Refresh Vote Totals';
           refreshButton.addEventListener('click', function(){
-            location.reload();
+            $.ajax({
+              url: 'https://bb-election-api.herokuapp.com/',
+              method: 'GET',
+              datatype: 'json'
+            }).done(function(data){
+              var updateVote = document.getElementsByTagName('span')
+              for (var i=0; i<data.candidates.length; i++){
+                updateVote[i].innerText = data.candidates[i].votes;
+              };
+            });
           });
           if (document.querySelector('button') === null){
           document.body.append(refreshButton)};

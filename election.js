8953b428baf8246e61a6a54be0bc77834b47dbb0
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-04 13:45:27 -0500

    completed synchronous request

--- election.js
+++ election.js
@@ -1,15 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-var im = document.querySelector('#im')
+var list = document.querySelector('#list');
   $.ajax({
     url: 'https://bb-election-api.herokuapp.com/',
     method: 'GET',
     datatype: 'json'
   }).done(function(data){
     for (i=0; i<data.candidates.length; i++){
-      var candidate = data.candidates[i]
+      var candidate = data.candidates[i];
       var liTag = document.createElement('li');
+      var formTag = document.createElement('form');
+      var submitButton = document.createElement('input');
+      var hiddenButton = document.createElement('input');
+      formTag.method = 'POST';
+      formTag.action = 'https://bb-election-api.herokuapp.com/vote';
+      // formTag.hiddenField = {'name': "name", 'value': candidate.name}
+      submitButton.type = "submit";
+      hiddenButton.setAttribute('type', "hidden");
+      hiddenButton.setAttribute('name', "name");
+      hiddenButton.setAttribute('value', candidate.name);
+      formTag.append(submitButton);
+      formTag.append(hiddenButton);
       liTag.innerText = candidate.name + ', Votes: ' + candidate.votes;
-      im.append(liTag)
-    }
-  })
+      liTag.append(formTag);
+      list.append(liTag);
+    };
+  });
 });
